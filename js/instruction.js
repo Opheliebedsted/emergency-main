a8d9cba02ff3a4b182982084efc4c06f677bd28f
Ophelie <opbe0001@tud.ek.dk>
2025-11-03 11:22:48 +0100

    Tilføjer ændringer i  farverne i sitet

--- js/instruction.js
+++ js/instruction.js
@@ -7,7 +7,9 @@ function fjollesyge() {
   console.log("button is here");
   document.querySelector("#fjollesyge").classList.add("my_scale");
   efficiency;
-  document.querySelector("#efficiency").textContent = " Hvad er fjollesyge";
+  document.querySelector("#efficiency").textContent = " Symptomer";
+  document.querySelector(".info-text h2").textContent =
+    "Risicoccus ludificans (fjollesyge-bakterien)";
 }
 
 document
@@ -17,4 +19,15 @@ document
 function changeColor() {
   console.log("changeColor is here");
   document.querySelector("#bidesyge").classList.add("changeColor");
+  document.querySelector(".info-text h2").textContent =
+    "Mordobacillus impulsivus (bidesyge-bakterien)";
+}
+
+document.querySelector("#babysyge_button").addEventListener("click", babysyge);
+
+function babysyge() {
+  console.log("babysyge button is here");
+  document.querySelector("#babysyge").classList.add("my_scale2");
+  document.querySelector(".info-text h2").textContent =
+    "Neonophilus natalis (babysyge-bakterien)";
 }
